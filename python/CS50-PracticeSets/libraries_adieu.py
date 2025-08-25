ffa85b9ab5fa2a89e8236f504b32811c865fbe86
Ahmad <Ahmadkaleeem1@gmail.com>
2025-08-26 00:35:01 +0500

    Added CS50x Python Problem of Libraraies

--- python/CS50-PracticeSets/libraries_adieu.py
+++ python/CS50-PracticeSets/libraries_adieu.py
@@ -1,16 +1,17 @@
-# In a file called adieu.py, implement a program that prompts the user for names, one per line, 
-# until the user inputs control-d. Assume that the user will input at least one name. 
+# In a file called adieu.py, implement a program that prompts the user for names, one per line,
+# until the user inputs control-d. Assume that the user will input at least one name.
 # Then bid adieu to those names, separating two names with one and, three names with two commas and one and, and 𝑛 names with 𝑛 −1 commas and one and.
-import inflect 
+import inflect
 bind = inflect.engine()
 names = []
 exitted = False
 
 while not exitted:
     try:
-     user_input = input("Name: ")
+     user_input = input()
      names.append(user_input)
     except EOFError:
       exitted = True
       pass
-print('Adieu, adieu, to ' + bind.join(names))
+names_joined = bind.join(names)
+print(f'Adieu, adieu, to {names_joined}' )
