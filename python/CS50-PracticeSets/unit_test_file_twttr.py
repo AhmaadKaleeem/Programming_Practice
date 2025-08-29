ec5adb2a99f54924b55ef3603f1f4700e9eba91a
Ahmad <Ahmadkaleeem1@gmail.com>
2025-08-30 03:40:08 +0500

    CS 50 Python Problem To Unit Code Test Our Program Solution

--- python/CS50-PracticeSets/unit_test_file_twttr.py
+++ python/CS50-PracticeSets/unit_test_file_twttr.py
@@ -2,4 +2,10 @@ from unit_test_twtr import shorten
 def test_vowels():
    for i in ["A","E","I","O","U","a","e","i","o","u"]:
     assert  shorten(i) == ""
- 
+def test_sentences():
+     assert shorten('Hello') == "Hll"
+     assert shorten('twitter') == "twttr"
+     assert shorten('AHMAD') == "HMD"
+     assert shorten('MAD1') == "MD1"
+     assert shorten('1Mad') == "1Md"
+     
