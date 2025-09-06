bc48aa616b8487bd388a90c10983147b020bb87b
Ahmad <Ahmadkaleeem1@gmail.com>
2025-09-06 13:43:28 +0500

    Saved Recording To File and Then Loaded Back

--- python/core-problems/sound_recorder.py
+++ python/core-problems/sound_recorder.py
@@ -6,12 +6,15 @@ import numpy as np
 fs = 44100
 time = int(input("Enter the Time You Want To Record: "))
 chanel = 1
+sound_file = "python/core-problems/sound.wav"
 print(f"Recording For {time} Seconds")
 record_data = sd.rec(int(fs*time),samplerate=fs,channels=chanel , dtype='float32')
 sd.wait()
 print("Recorded For {time} Seconds")
-print("Playing Recording ........")
+sf.write(sound_file,record_data,fs)
 
-sd.play(record_data,fs)
+print("Playing Recording Back ........")
+loadback_data,loadback_fs = sf.read(sound_file,dtype='float32')
+sd.play(loadback_data,fs)
 sd.wait()
 print('Playback Done')
