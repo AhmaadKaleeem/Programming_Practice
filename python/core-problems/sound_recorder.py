ad8f109cda33a7595db29bd5d9e72b6a00a608d6
Ahmad <Ahmadkaleeem1@gmail.com>
2025-09-06 13:54:56 +0500

    Saved Recording To File and Asked User For Playback

--- python/core-problems/sound_recorder.py
+++ python/core-problems/sound_recorder.py
@@ -1,5 +1,6 @@
 import sounddevice as sd
 import soundfile as sf
+import sys
 import numpy as np
 
 # Recording Fixed 3 sec wave
@@ -10,11 +11,18 @@ sound_file = "python/core-problems/sound.wav"
 print(f"Recording For {time} Seconds")
 record_data = sd.rec(int(fs*time),samplerate=fs,channels=chanel , dtype='float32')
 sd.wait()
-print("Recorded For {time} Seconds")
+print(f"Recorded For {time} Seconds and Saved at {sound_file}")
 sf.write(sound_file,record_data,fs)
-
-print("Playing Recording Back ........")
-loadback_data,loadback_fs = sf.read(sound_file,dtype='float32')
-sd.play(loadback_data,fs)
-sd.wait()
-print('Playback Done')
+# Asking User Either They Want To Playback or Not
+try:
+ option = int(input("Do You Want To Playback? \n1. Yes \n2.No \nPlease Select: " ))
+ if option == 1:
+  print("Playing Recording Back ........")
+  loadback_data,loadback_fs = sf.read(sound_file,dtype='float32')
+  sd.play(loadback_data,fs)
+  sd.wait()
+  print('Playback Done')
+ elif option == 2:
+     sys.exit("Exiting The Program.......")
+except ValueError:
+    sys.exit(0)
