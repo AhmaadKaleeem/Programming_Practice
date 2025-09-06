2f076e67e2570f08b8a78cd545883138ccf72455
Ahmad <Ahmadkaleeem1@gmail.com>
2025-09-06 13:19:49 +0500

    Crated and Runned a sample tone

--- python/core-problems/sound_recorder.py
+++ python/core-problems/sound_recorder.py
@@ -2,3 +2,13 @@ import sounddevice as sd
 import soundfile as sf
 import numpy as np
 
+# Creating a sample wave
+fs = 44100
+time = 1
+t = np.linspace(0,time,int(fs*time),endpoint=False)
+audio_wave = 0.5 * np.sin(2 * np.pi * 440 * t)
+
+print("Playing Tone ........")
+sd.play(audio_wave,fs)
+sd.wait()
+print('Done')
