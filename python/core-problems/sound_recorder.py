67c5f2c4503f6988b919538597778490c5472995
Ahmad <Ahmadkaleeem1@gmail.com>
2025-09-06 13:32:31 +0500

    Recorded for dynmic user based time nd played back

--- python/core-problems/sound_recorder.py
+++ python/core-problems/sound_recorder.py
@@ -4,13 +4,14 @@ import numpy as np
 
 # Recording Fixed 3 sec wave
 fs = 44100
-time = 3
+time = int(input("Enter the Time You Want To Record: "))
 chanel = 1
 print(f"Recording For {time} Seconds")
 record_data = sd.rec(int(fs*time),samplerate=fs,channels=chanel , dtype='float32')
 sd.wait()
 print("Recorded For {time} Seconds")
 print("Playing Recording ........")
+
 sd.play(record_data,fs)
 sd.wait()
 print('Playback Done')
